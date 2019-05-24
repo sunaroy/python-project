8e99c6f7320b7369d634ca9687b3232fc8ac7efb
sunanda <sunanda.roy06@gmail.com>
2019-05-24 10:20:00 +0530

    added user registration

--- flask-application/app.py
+++ flask-application/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
@@ -62,6 +63,7 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     app.run(debug=True)  # important to mention debug=True
--- flask-application/user.py
+++ flask-application/user.py
@@ -1,5 +1,77 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+
+
 class User(object):
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
         self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+    	connection = sqlite3.connect('data.db')
+    	cursor = connection.cursor()
+
+
+    	query = ("SELECT * FROM users WHERE username = ?")
+
+    	row = cursor.execute(query, (username,))
+
+    	if row:
+    		print("********************88888888{}".format(row[1]))
+    		user = cls(*row) #here cls is User
+    	else:
+    		user = none
+
+    	connection.close()
+
+    	return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+    	connection = sqlite3.connect('data.db')
+    	cursor = connection.cursor()
+
+
+    	query = ("SELECT * FROM users WHERE id = ?")
+
+    	row = cursor.execute(query, (_id,))
+
+    	if row:
+    		user = cls(row[0], row[1], row[2]) #here cls is User
+    	else:
+    		user = none
+
+    	connection.close()
+
+    	return user
+
+
+
+
+class UserRegister(Resource):
+	parser = reqparse.RequestParser()
+	parser.add_argument("username",
+		type = str,							
+		required=True,
+		help="This field cannot be blank"
+	)
+
+	parser.add_argument("password",
+		type=str,
+		required=True,
+		help="This field cannot be left blank"
+	)
+
+	def post(self):
+		data = UserRegister.parser.parse_args()
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+
+		insert_query = "INSERT INTO users VALUES(NULL, ?, ?)"
+
+		cursor.execute(insert_query, (data["username"], data["password"]))
+		connection.commit()
+		connection.close()
+		return {"message": "User created successfully"}, 201
